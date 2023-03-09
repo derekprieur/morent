185905e6832e91b7f4a2688ca6ba73ce0eccd581
Derek Prieur <DPrieur123@gmail.com>
2023-03-09 16:33:19 -0500

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,49 @@
-import { morent } from '../assets'
+import { morent, morentLarge } from '../assets'
 
 const Footer = () => {
     return (
-        <div className='bg-[#F6F7F9] px-6 pt-3'>
-            <img src={morent} alt="logo" className='pb-4' />
-            <p className='text-[#90A3BF] max-w-[65%] pb-12'>Our vision is to provide convenience and help increase your sales business.</p>
-            <div className='flex justify-between'>
-                <div className='flex-1'>
-                    <h3 className='text-[#1A202C] font-bold text-xl mb-4'>About</h3>
-                    <ul className='gap-3 flex flex-col text-[#90A3BF] text-lg'>
-                        <li>How it works</li>
-                        <li>Featured</li>
-                        <li>Partnership</li>
-                        <li>Business Relation</li>
-                    </ul>
+        <div className='bg-[#F6F7F9] md:bg-white px-6 md:px-[60px] pt-3 md:pt-[60px]'>
+            <div className='flex flex-col md:flex-row justify-between'>
+                <div>
+                    <img src={morent} alt="logo" className='pb-4 flex md:hidden' />
+                    <img src={morentLarge} alt="logo" className='pb-8 hidden md:flex' />
+                    <p className='text-[#90A3BF] max-w-[65%] pb-12 md:text-lg'>Our vision is to provide convenience and help increase your sales business.</p>
                 </div>
-                <div className='flex-1'>
-                    <h3 className='text-[#1A202C] font-bold text-xl mb-4'>Socials</h3>
-                    <ul className='gap-3 flex flex-col text-[#90A3BF] text-lg'>
-                        <li>How it works</li>
-                        <li>Featured</li>
-                        <li>Partnership</li>
-                        <li>Business Relation</li>
-                    </ul>
+                <div className='flex gap-16 flex-wrap'>
+                    <div>
+                        <h3 className='text-[#1A202C] font-bold text-xl mb-4'>About</h3>
+                        <ul className='gap-3 flex flex-col text-[#90A3BF] text-lg'>
+                            <li>How it works</li>
+                            <li>Featured</li>
+                            <li>Partnership</li>
+                            <li>Business Relation</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className='text-[#1A202C] font-bold text-xl mb-4'>Socials</h3>
+                        <ul className='gap-3 flex flex-col text-[#90A3BF] text-lg'>
+                            <li>Discord</li>
+                            <li>Instagram</li>
+                            <li>Twitter</li>
+                            <li>Facebook</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 className='text-[#1A202C] font-bold text-xl mb-4'>Community</h3>
+                        <ul className='gap-3 flex flex-col text-[#90A3BF] text-lg'>
+                            <li>Events</li>
+                            <li>Blog</li>
+                            <li>Podcast</li>
+                            <li>Invite a friend</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
+            <div className='flex justify-between mt-12 font-bold text-sm'>
+                <p>Privacy & Policy</p>
+                <p>Terms & Conditions</p>
+            </div>
+            <p className='font-bold text-sm mt-8 pb-6'>©2022 MORENT. All rights reserved</p>
         </div>
     )
 }
