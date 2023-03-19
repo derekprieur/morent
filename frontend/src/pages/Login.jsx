4e0c6308cb8b198864f0049e45bb7f494fc006b5
Derek Prieur <DPrieur123@gmail.com>
2023-03-18 20:28:19 -0400

    updates

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -61,53 +61,55 @@ const Login = () => {
     }
 
     return (
-        <div className='h-screen max-w-6xl w-full flex px-6 justify-center'>
-            {showSignUp ? (
-                <form className='flex flex-col gap-4 w-full' onSubmit={handleSignUp}>
-                    <h4 className='mt-8 text-[#3563E9] text-xl font-extrabold mb-6'>Sign Up</h4>
-                    <p className='text-sm'>Already have an account? <span className='text-[#3D5278] cursor-pointer' onClick={handleSignUpClick}>Sign in</span></p>
-                    <div className='flex flex-col'>
-                        <h3 className='font-medium text-lg mb-4'>Username</h3>
-                        <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
-                    </div>
-                    <div className='flex flex-col'>
-                        <h3 className='font-medium text-lg mb-4'>Password</h3>
-                        <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
-                    </div>
-                    <div className='flex flex-col'>
-                        <h3 className='font-medium text-lg mb-4'>First Name</h3>
-                        <input type="text" placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
-                    </div>
-                    <div className='flex flex-col'>
-                        <h3 className='font-medium text-lg mb-4'>Last Name</h3>
-                        <input type="text" placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
-                    </div>
-                    <div className='flex flex-col'>
-                        <h3 className='font-medium text-lg mb-4'>Title</h3>
-                        <input type="text" placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
-                    </div>
-                    <div className='flex flex-col'>
-                        <h3 className='font-medium text-lg mb-4'>Profile Picture</h3>
-                        <input type="file" accept="image/*" onChange={(e) => setFile(e.target.files[0])} />
-                    </div>
-                    <Button text='Sign Up' size='large' rounded full type='submit' />
-                </form>
-            ) : (
-                <form type='submit' className='flex flex-col gap-4 w-full' onSubmit={handleLogin}>
-                    <h4 className='mt-8 text-[#3563E9] text-xl font-extrabold mb-6'>Login</h4>
-                    <p className='text-sm'>Don't have an account? <span className='text-[#3D5278] cursor-pointer' onClick={handleSignUpClick}>Sign up</span></p>
-                    {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
-                    <div className='flex flex-col'>
-                        <h3 className='font-medium text-lg mb-4'>Username</h3>
-                        <input type="text" placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
-                    </div>
-                    <div className='flex flex-col'>
-                        <h3 className='font-medium text-lg mb-4'>Password</h3>
-                        <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
-                    </div>
-                    <Button text='Login' size='large' rounded full type='submit' />
-                </form>
-            )}
+        <div className='h-screen max-w-xl w-full flex px-6 mx-auto'>
+            <div className='w-full'>
+                {showSignUp ? (
+                    <form className='flex flex-col gap-4 w-full' onSubmit={handleSignUp}>
+                        <h4 className='mt-8 text-[#3563E9] text-xl font-extrabold mb-6'>Sign Up</h4>
+                        <p className='text-sm'>Already have an account? <span className='text-[#3D5278] cursor-pointer' onClick={handleSignUpClick}>Sign in</span></p>
+                        <div className='flex flex-col'>
+                            <h3 className='font-medium text-lg mb-4'>Username</h3>
+                            <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
+                        </div>
+                        <div className='flex flex-col'>
+                            <h3 className='font-medium text-lg mb-4'>Password</h3>
+                            <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
+                        </div>
+                        <div className='flex flex-col'>
+                            <h3 className='font-medium text-lg mb-4'>First Name</h3>
+                            <input type="text" placeholder='First Name' value={firstName} onChange={(e) => setFirstName(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
+                        </div>
+                        <div className='flex flex-col'>
+                            <h3 className='font-medium text-lg mb-4'>Last Name</h3>
+                            <input type="text" placeholder='Last Name' value={lastName} onChange={(e) => setLastName(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
+                        </div>
+                        <div className='flex flex-col'>
+                            <h3 className='font-medium text-lg mb-4'>Title</h3>
+                            <input type="text" placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
+                        </div>
+                        <div className='flex flex-col'>
+                            <h3 className='font-medium text-lg mb-4'>Profile Picture</h3>
+                            <input type="file" accept="image/*" onChange={(e) => setFile(e.target.files[0])} />
+                        </div>
+                        <Button text='Sign Up' size='large' rounded full type='submit' />
+                    </form>
+                ) : (
+                    <form type='submit' className='flex flex-col gap-4 w-full' onSubmit={handleLogin}>
+                        <h4 className='mt-8 text-[#3563E9] text-xl font-extrabold mb-6'>Login</h4>
+                        <p className='text-sm'>Don't have an account? <span className='text-[#3D5278] cursor-pointer' onClick={handleSignUpClick}>Sign up</span></p>
+                        {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+                        <div className='flex flex-col'>
+                            <h3 className='font-medium text-lg mb-4'>Username</h3>
+                            <input type="text" placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
+                        </div>
+                        <div className='flex flex-col'>
+                            <h3 className='font-medium text-lg mb-4'>Password</h3>
+                            <input type="password" placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)} required className='rounded-md md:rounded-xl bg-[#F6F7F9] px-4 md:px-8 py-4 placeholder:font-light' />
+                        </div>
+                        <Button text='Login' size='large' rounded full type='submit' />
+                    </form>
+                )}
+            </div>
         </div>
     )
 }
