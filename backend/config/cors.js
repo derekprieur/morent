50034a5c888a0a3ddfd671ff67f03a04b29d2c9b
Derek Prieur <DPrieur123@gmail.com>
2023-03-30 09:17:30 -0400

    update

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: ['http://127.0.0.1:5173', 'https://morent-dprieur.vercel.app/'],
+    origin: ['http://127.0.0.1:5173', 'https://morent-dprieur.vercel.app'],
 };
 
 module.exports = cors(corsOptions);
